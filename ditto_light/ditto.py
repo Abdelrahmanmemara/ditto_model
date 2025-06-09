42da6b40364815b73f17703046d8d3ba1a8ba2a0
Abdelrahmanmemara <119069265+Abdelrahmanmemara@users.noreply.github.com>
2025-06-09 11:44:25 +0200

    Update ditto.py
    Solved the can not import error

--- ditto_light/ditto.py
+++ ditto_light/ditto.py
@@ -11,7 +11,8 @@ import argparse
 
 from .dataset import DittoDataset
 from torch.utils import data
-from transformers import AutoModel, AdamW, get_linear_schedule_with_warmup
+from transformers import AutoModel, get_linear_schedule_with_warmup
+from transformers.optimization import AdamW
 from tensorboardX import SummaryWriter
 from apex import amp
 
