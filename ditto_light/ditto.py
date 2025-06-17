709589fe11c808b4f7b9b2999dd5a3544ad042c0
Abdelrahmanmemara <119069265+Abdelrahmanmemara@users.noreply.github.com>
2025-06-17 10:58:57 +0200

    Update ditto.py

--- ditto_light/ditto.py
+++ ditto_light/ditto.py
@@ -15,7 +15,7 @@ from transformers import AutoModel, get_linear_schedule_with_warmup
 # from transformers.optimization import AdamW
 from torch.optim import AdamW
 from tensorboardX import SummaryWriter
-from apex import amp
+# from apex import amp
 
 lm_mp = {'roberta': 'roberta-base',
          'distilbert': 'distilbert-base-uncased'}
@@ -134,9 +134,9 @@ def train_step(train_iter, model, optimizer, scheduler, hp):
 
         loss = criterion(prediction, y.to(model.device))
 
-        if hp.fp16:
-            with amp.scale_loss(loss, optimizer) as scaled_loss:
-                scaled_loss.backward()
+        # if hp.fp16:
+        #     with amp.scale_loss(loss, optimizer) as scaled_loss:
+        #         scaled_loss.backward()
         else:
             loss.backward()
         optimizer.step()
@@ -186,8 +186,8 @@ def train(trainset, validset, testset, run_tag, hp):
     model = model.cuda()
     optimizer = AdamW(model.parameters(), lr=hp.lr)
 
-    if hp.fp16:
-        model, optimizer = amp.initialize(model, optimizer, opt_level='O2')
+    # if hp.fp16:
+    #     model, optimizer = amp.initialize(model, optimizer, opt_level='O2')
     num_steps = (len(trainset) // hp.batch_size) * hp.n_epochs
     scheduler = get_linear_schedule_with_warmup(optimizer,
                                                 num_warmup_steps=0,
